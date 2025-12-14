f0a07b0735e6ac9a1e026d677e0fc86dc3e55e67
Phone Waiyam Hein <phonewaiyamhein@gmail.com>
2025-12-14 16:04:03 +0630

    fix: clean up codebase

--- design/generators/run.sh
+++
@@ -1,4 +0,0 @@
-node ./design/generators/assets.js
-
-
-lua ./design/generators/json_to_lua.lua ./design/generators/images.json ./design/generators/images.lua
--- design/helpers/parser.sh
+++
@@ -1,8 +0,0 @@
-lua ./helpers/json_to_lua.lua data/scenes/ch0.json data/static/scenes/ch0.lua
-lua ./json_to_lua.lua data/scenes/ch0.json data/static/scenes/ch0.lua
-
-lua ./json_to_lua.lua zones.json zones.lua
-
-lua ./json_to_lua.lua time_periods.json time_periods.lua
-
-lua ./json_to_lua.lua time_periods_array.json time_periods_array.lua
